f7dea73699e90be1c727915f26d5ee34693d55d8
Reisson Saavedra <reisson.saavedra.miguel@gmail.com>
2022-02-24 01:07:36 -0500

    Ready

--- client/src/Components/InfoUser.js
+++ client/src/Components/InfoUser.js
@@ -2,7 +2,7 @@ import { Component } from "react";
 export default class InfoUser extends Component{
     render(){
         return <div className='info_user' >
-        <img src={this.props.dataUser.avatar_url} className='avatar'></img>
+        <img src={this.props.dataUser.avatar_url} className='avatar' alt={this.props.dataUser.login}></img>
           <h4>{this.props.dataUser.name}</h4>
           <h4>{this.props.dataUser.location}</h4>
           {this.props.dataUser.bio}
